9bad6314b4954b71b5712f5221f92bf85875bf35
AnanthAkshay <ananthakshay2006@gmail.com>
2026-08-21 18:44:05 +0530

    Add app/ios/Runner/Base.lproj/Main.storyboard and related components

---
+++ app/lib/core/constants/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String defaultApiBaseUrl = 'http://localhost:8085/api/v1';
+  static const String appName = 'Expense Tracker';
+  static const String currencySymbol = '₹';
+}
